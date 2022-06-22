626e289f86a68c7a0a675bf7fd57c8bbb9ed619d
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-06-22 16:55:38 +0100

    jsx conditionals

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ function App() {
     { id: 2, text: 'comment two' },
     { id: 3, text: 'comment three' },
   ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading....</h1>;
+  //can use ternary operator if showcomments true, output jsx, if no return 'no' or null. otherwise could use AND operator to short circuit this.
+
+  const commentBlock = (
+    <div className='comments'>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className='container'>
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div className='comments'>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+
+      {showComments && commentBlock}
     </div>
   );
 }
