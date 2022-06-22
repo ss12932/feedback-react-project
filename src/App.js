86de7c51deca6e13829186fc629ede970aa152d9
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-06-22 22:26:49 +0100

    card component + conditional styles

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/feedbackData';
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'this is my blog post';
-  const comments = [
-    { id: 1, text: 'comment one' },
-    { id: 2, text: 'comment two' },
-    { id: 3, text: 'comment three' },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading....</h1>;
-  //can use ternary operator if showcomments true, output jsx, if no return 'no' or null. otherwise could use AND operator to short circuit this.
-
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className='container'></div>
+      <FeedbackList feedback={feedback} />
+    </>
   );
 }
 
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from './FeedbackItem';
+
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet!</p>;
+  }
+  console.log(feedback);
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedbackList;
