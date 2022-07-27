985ec631e7c586f8ec9fe331eb38db5fa73df427
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-27 14:37:08 +0100

    added real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,19 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
   const handleTextChange = (e) => {
+    if (text === '') {
+      setbtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setbtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setMessage(null);
+      setbtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -19,8 +31,11 @@ function FeedbackForm() {
             placeholder='write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
