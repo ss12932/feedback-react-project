8d3b3ec055df8b8a2d676b81b4b4b70dfe86141c
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-01 13:22:45 +0100

    link to about page created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 import FeedbackData from './data/feedbackData';
 
@@ -42,6 +43,7 @@ function App() {
           ></Route>
           <Route path='/about' element={<AboutPage />} />
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
   );
