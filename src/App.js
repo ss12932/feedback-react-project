eddc609cf90c83e41d755302f7f2eee574f54501
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-01 16:06:55 +0100

    added test nested routes

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
             }
           ></Route>
           <Route path='/about' element={<AboutPage />} />
-          <Route path='/post/:id/:name' element={<Post />} />
+          <Route path='/post/*' element={<Post />} />
         </Routes>
         <AboutIconLink />
       </div>
